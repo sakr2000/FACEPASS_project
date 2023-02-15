ae7a7e5f958e8d241dec55bae14ffd1c9b930de9
mohamed_sakr <sakr83117@gmail.com>
2023-02-16 00:01:21 +0200

    Finished login : )

--- HTML/login_page/login.js
+++ HTML/login_page/login.js
@@ -1,6 +1,12 @@
-email = document.getElementById("email");
-password = document.getElementById("password");
-login = document.getElementById("login");
+username = document.getElementById("username");
+signup_email = document.getElementById("signup_email");
+signup_pass = document.getElementById("signup_password");
+repassword = document.getElementById("repassword");
+signup_btn = document.getElementById("signup_btn");
+login_email = document.getElementById("login_email");
+login_pass = document.getElementById("login_password");
+login_btn = document.getElementById("login_btn");
+
 container = document.getElementById("container");
 registerLink = document.getElementById("registerLink");
 loginLink = document.getElementById("loginLink");
@@ -12,12 +18,51 @@ loginLink.addEventListener("click", () => {
   container.classList.remove("active");
 });
 
-// login.addEventListener("click", () => {
-//   chrome.storage.local.set({ email: email.value });
-//   chrome.storage.local.set({ password: password.value });
+// adding new User
+let users = [];
+signup_btn.addEventListener("click", () => {
+  if (signup_pass.value == repassword.value) {
+    let user = {
+      name: username.value,
+      email: signup_email.value,
+      password: signup_pass.value,
+      active: false,
+    };
+    users.push(user);
+    console.log(users);
+    chrome.storage.local.set({ users: users });
+    chrome.storage.local.get(["users"]).then((result) => {
+      console.log(JSON.stringify(result.users));
+    });
+    container.classList.remove("active");
+  } else {
+    alert("The two passwords are not the same");
+  }
+});
 
-//   chrome.storage.local.get(["email"]).then((result) => {
-//     console.log("Value currently is " + result.email);
-//   });
-// });
-// console.log(email, password);
+chrome.storage.onChanged.addListener((changes, namespace) => {
+  for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
+    console.log(
+      `Storage key "${key}" in namespace "${namespace}" changed.`,
+      `Old value was "${oldValue}", new value is "${newValue}".`
+    );
+  }
+});
+
+// logging in
+login_btn.addEventListener("click", () => {
+  chrome.storage.local.get(["users"]).then((result) => {
+    for (let i = 0; i < result.users.length; i++) {
+      const element = result.users[i];
+      console.log(element);
+      if (
+        (element.email == login_email.value) &
+        (element.password == login_pass.value)
+      ) {
+        result.users[i].active = true;
+        chrome.storage.local.set({ users: result.users });
+        window.location.href = "../main page/main.html";
+      }
+    }
+  });
+});
--- HTML/main page/js/main.js
+++ HTML/main page/js/main.js
@@ -1,9 +1,20 @@
 settingbtn = document.getElementById("settingbtn");
 sidebar = document.getElementById("sidebar");
+username = document.getElementById("username");
 closesidebar = document.getElementById("closesidebar");
 settingbtn.addEventListener("click", function () {
   sidebar.classList.add("active");
 });
 closesidebar.addEventListener("click", function () {
   sidebar.classList.remove("active");
+});
+
+chrome.storage.local.get(["users"]).then((result) => {
+  for (let i = 0; i < result.users.length; i++) {
+    const element = result.users[i];
+    console.log(element);
+    if (element.active == true) {
+      username.innerHTML = element.name;
+    }
+  }
 });
