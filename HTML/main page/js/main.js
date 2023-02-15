e65392cdc8fd8b3217d5d58b9c3afcc8383b74ad
ghada <ghadahussein25@gmail.com>
2023-02-16 00:10:05 +0200

    setting page

--- HTML/main page/js/main.js
+++ HTML/main page/js/main.js
@@ -1,6 +1,16 @@
 settingbtn = document.getElementById("settingbtn");
 sidebar = document.getElementById("sidebar");
 closesidebar = document.getElementById("closesidebar");
+
+account = document.getElementById("acc").onclick = function() {
+  window.open("setting page/acc.html", "_blank");
+}
+
+
+password = document.getElementById("pass").onclick = function(){
+  window.open("setting page/pass.html", "_blank");
+};
+
 settingbtn.addEventListener("click", function () {
   sidebar.classList.add("active");
 });
