4384c8693eb973bae8a7ba48ad091f481f1481e2
ghada <ghadahussein25@gmail.com>
2023-02-26 15:59:10 +0200

    change name and password

--- HTML/main page/js/acc.js
+++ HTML/main page/js/acc.js
@@ -1,62 +1,179 @@
 account = document.getElementById("account");
 password = document.getElementById("password");
 displayElement = document.getElementById("displayElement");
+password_page = document.getElementById("password_page");
+account_page = document.getElementById("account_page");
 
-account.onclick = function() {
-    displayElement.innerHTML =`
-    <div class="photo" id="photo">
-    <img src="../images/user.png">
-        <div>
-            <label for="file"> Change Photo</label>
-            <input id="file" type="file" accept="image/png, image/jpeg, image/jpg" style="display:none;">
-        </div>
-    </div>
+account.onclick = () => {
+    account_page.style.display="block";
+    password_page.style.display="none";
+    account.style.color ="#077545";
+    account.style.backgroundColor ="white";
+    account.style.borderRadius = "20px 0px 0px 20px";
+    password.style = "none";
+}
 
-    <form id="form">
-        <div class="name">
-            <div class="Fname">
-                <label>First Name</label>
-                <input type="text">
-            </div>
+password.onclick = () => {
+    account_page.style.display="none";
+    password_page.style.display="block";
+    password.style.color ="#077545";
+    password.style.backgroundColor ="white";
+    password.style.borderRadius = "20px 0px 0px 20px";
+    account.style = "none";
+}
 
-            <div class="Lname">
-                <label>Last Name</label>
-                <input type="text">
-            </div>
-        </div>
+/** =========================================================================================
+    ========================================================================================= **/
 
-        <div class="email">
-            <label>Email Address</label>
-            <input type="email">
-        </div>
+    first_name = document.getElementById("first_name");
+    last_name = document.getElementById("last_name");
+    save_btn = document.getElementById("save_btn");
 
-        <div class="btn">
-            <input type="submit" value="Save Changes">
-        </div>
-    </form>`;
+const setError = (element, message) => {
+    const parent = element.parentElement;
+    const error = parent.querySelector(".error");
+    error.innerText = message;
+    element.classList.add("invalid");
+};
+const setValid = (element) => {
+    const parent = element.parentElement;
+    const error = parent.querySelector(".error");
+    error.innerText = "";
+    element.classList.remove("invalid");
 };
 
-password.onclick = function() {
-    displayElement.innerHTML = `
-    <div class="photo2">
-        <img src="../images/password-logo.png">
-    </div>
-    <div class="password">
-        <div class="current">
-            <label for="current">Current Password</label>
-            <input id="current" type="password">
-        </div>
-        <div class="new">
-            <label for="new">New Password</label>
-            <input id="new" type="password">
-        </div>
-        <div class="confirm">
-        <label for="confirm">Confirm Password</label>
-            <input id="confirm" type="password">
-        </div>
-    </div>
-        <div class="btn2">
-            <button type="submit">Save</button>
-        </div>`;
+function checkFirstname() {
+    let re = /^[a-zA-Z][a-zA-Z0-9]{2,50}$/;
+    let name = first_name.querySelector("input").value;
+    if (name == "") {
+    setError(first_name, "field can't be empty");
+    } else if (!name.match(re)) {
+    setError(first_name, "name not valid ");
+    } else {
+    setValid(first_name);
+    }
 };
+
+function checkLastname() {
+    let re = /^[a-zA-Z][a-zA-Z0-9]{2,50}$/;
+    let name = last_name.querySelector("input").value;
+    if (name == "") {
+    setError(last_name, "field can't be empty");
+    } else if (!name.match(re)) {
+    setError(last_name, "name not valid ");
+    } else {
+    setValid(last_name);
+    }
+};
+
+first_name.querySelector("input").onkeyup = () => {
+    checkFirstname();
+};
+last_name.querySelector("input").onkeyup = () => {
+    checkLastname();
+};
+
+//====================================================================================//
+
+current_password = document.getElementById("current_password");
+new_password = document.getElementById("new_password");
+confirm_password = document.getElementById("confirm_password");
+pass_btn = document.getElementById("pass_btn");
+
+function currentpassword() {
+    chrome.storage.local.get(["users"]).then((result) => {
+        for(let i = 0; i < result.users.length; i++) {
+            const element = result.users[i];
+            if(element.active == true) {
+                if(current_password.querySelector("input").value != element.password) {
+                    setError(current_password, "Password doesn't match ");
+                } else {
+                    setValid(current_password);
+                }
+            }
+        }
+    });
+};
+
+function newpassword() {
+    let pass = new_password.querySelector("input").value;
+    if (pass == "") {
+    setError(new_password, "Password can't be empty");
+    } else if (pass.length < 8) {
+    setError(new_password, "Password length must be greater than 8");
+    } else {
+    setValid(new_password);
+    }
+};
+
+function confirmpassword() {
+    if (
+    new_password.querySelector("input").value !=
+    confirm_password.querySelector("input").value
+    ) {
+    setError(confirm_password, "Password doesn't match");
+    } else if (confirm_password.querySelector("input").value == "") {
+    setError(confirm_password, "This field can't be empty");
+    } else {
+    setValid(confirm_password);
+    }
+};
+
+current_password.querySelector("input").onkeyup = () => {
+    currentpassword();
+};
+new_password.querySelector("input").onkeyup = () => {
+    newpassword();
+};
+confirm_password.querySelector("input").onblur = () => {
+    confirmpassword();
+};
+//==================================================================================================//
+//change username
+save_btn.addEventListener("click", () => {
+    checkFirstname();
+    checkLastname();
+    
+    if(!first_name.classList.contains("invalid") &&
+        !last_name.classList.contains("invalid"))
+    {
+        let username = first_name.querySelector("input").value + last_name.querySelector("input").value;
+        chrome.storage.local.get(["users"]).then((result) => {
+            console.log(result.users);
+            for (let i = 0; i < result.users.length; i++) {
+                const element = result.users[i];
+                if(element.active == true) {
+                    element.name = username;
+                }
+                console.log("new name: " + element.name);
+                chrome.storage.local.set({users: result.users});
+            }
+        });
+    }
+});
+
+//=================================================================================================//
+//change password
+pass_btn.addEventListener("click", () => {
+    currentpassword();
+    newpassword();
+    confirmpassword();
+    let element;
+    if(!current_password.classList.contains("invallid") &&
+        !new_password.classList.contains("invalid") &&
+        !confirm_password.classList.contains("invalid"))
+        {
+            chrome.storage.local.get(["users"]).then((result) => {
+                console.log(result.users);
+                for(let i = 0; i < result.users.length; i++) {
+                    element = result.users[i];
+                    if(element.active == true) {
+                        element.password = confirm_password.querySelector("input").value;
+                    }
+                    console.log("new password: " + element.password);
+                    chrome.storage.local.set({users: result.users});
+                }
+            });
+        }
+});
 
