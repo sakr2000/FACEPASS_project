044ff863ac87514a6475595b2635a43c56bc3188
mohamed_sakr <sakr83117@gmail.com>
2023-07-16 09:20:16 +0300

    Design edits

--- HTML/main page/setting page/acc.js
+++ HTML/main page/setting page/acc.js
@@ -189,3 +189,17 @@ pass_btn.addEventListener("click", () => {
     });
   }
 });
+
+// collapsible elements
+let coll = document.getElementsByClassName("collapsible");
+for (let i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
